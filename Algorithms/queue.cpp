043b2094a16f251c0185a57124f502edfec6d77f
ZBM <520140341@qq.com>
2016-03-25 22:24:14 +0800

    2016-3-25

--- Algorithms/queue.cpp
+++ Algorithms/queue.cpp
@@ -3,9 +3,73 @@
 */
 
 #include<iostream>
+#include<stack>
 using namespace std;
+
+int pop_data[100];
+
+class queue
+{
+private:
+    int count;
+    stack<int> s;
+public:
+    queue();
+    void push(int data);
+    void pop();
+    int empty();
+};
+
+queue::queue():count(0)
+{
+
+}
+
+void queue::push(int data)
+{
+    if(s.empty())
+    {
+        s.push(data);
+    }
+    else
+    {
+       for(int i=0;i<count;i++)
+       {
+           pop_data[i]=s.top();
+           s.pop();
+       }
+       s.push(data);
+       for(int i=count-1;i>=0;i--)
+       {
+           s.push(pop_data[i]);
+       }
+    }
+    count++;
+}
+
+void queue::pop()
+{
+    cout<<s.top()<<endl;
+    s.pop();
+    count--;
+}
+
+int queue::empty()
+{
+    if(count==0){return 0;}
+    else{return 1;}
+}
 
 int main()
 {
+    pop_data[100]=0;
+    queue q=queue();
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.pop();
+    q.pop();
+    q.pop();
+    cout<<q.empty();
     return 0;
 }
